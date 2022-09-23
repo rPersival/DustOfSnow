c7e4cd5c244b6d32b67c8db9e63fd1bf098f275f
rPersival <ghof460@gmail.com>
2022-09-24 02:29:04 +0300

    Setup mod

--- src/main/java/com/rpersival/snowdust/mixin/DustOfSnowMixin.java
+++ src/main/java/com/rpersival/snowdust/mixin/DustOfSnowMixin.java
@@ -1,6 +1,6 @@
-package net.fabricmc.example.mixin;
+package com.rpersival.snowdust.mixin;
 
-import net.fabricmc.example.ExampleMod;
+import com.rpersival.snowdust.DustOfSnow;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -8,9 +8,9 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
-public class ExampleMixin {
+public class DustOfSnowMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		DustOfSnow.LOGGER.info("This line is printed by the Dust of Snow mixin!");
 	}
 }
